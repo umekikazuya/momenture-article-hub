155b8538a801c8fd7d9619788aec474463da45b8
Kazuya Umeki <108924738+umekikazuya@users.noreply.github.com>
2025-07-26 09:25:05 +0900

    feat: 永続化実装、Create

--- internal/infrastructure/persistence/postgres/article_repository_impl.go
+++ internal/infrastructure/persistence/postgres/article_repository_impl.go
@@ -3,16 +3,31 @@ package postgres
 import (
 	"context"
 	"errors"
+	"fmt"
 
 	"github.com/umekikazuya/momenture-article-hub/internal/domain/entity"
 	"github.com/umekikazuya/momenture-article-hub/internal/domain/repository"
+	"gorm.io/gorm"
 )
 
 type PostgresArticleRepository struct {
+	postgresDBClient *gorm.DB
 }
 
-func NewPostgresArticleRepository() repository.ArticleRepository {
-	return &PostgresArticleRepository{}
+type articleModel struct {
+	gorm.Model
+	id            uint64
+	title         string
+	body          string
+	status        string
+	provider_type string
+	link          string
+}
+
+func NewPostgresArticleRepository(postgresDBClient *gorm.DB) repository.ArticleRepository {
+	return &PostgresArticleRepository{
+		postgresDBClient: postgresDBClient,
+	}
 }
 
 // FindAll は全ての記事を取得する
@@ -22,6 +37,9 @@ func (r *PostgresArticleRepository) FindAll(ctx context.Context) ([]*entity.Arti
 
 // FindByID は指定されたIDの記事を取得する
 func (r *PostgresArticleRepository) FindByID(ctx context.Context, id uint64) (*entity.Article, error) {
+	var articleModel []articleModel
+	r.postgresDBClient.Find(&articleModel).First(&articleModel)
+	fmt.Println(articleModel)
 	return nil, errors.New("not implemented")
 }
 
@@ -32,6 +50,12 @@ func (r *PostgresArticleRepository) FindByCriteria(ctx context.Context, criteria
 
 // Create は新しい記事を作成する
 func (r *PostgresArticleRepository) Create(ctx context.Context, article *entity.Article) (*entity.Article, error) {
+	articleModel := articleModel{
+		title:  article.Title.String(),
+		status: article.Status.String(),
+	}
+	r.postgresDBClient.Create(&articleModel)
+	fmt.Println("作成", articleModel)
 	return nil, errors.New("not implemented")
 }
 
